bb364179312dcdb212003ffa36a6143a6d20dd5d
Cory Hrycko <chrycko9491@gmail.com>
2019-03-13 23:51:19 -0400

    adding some resources onto the blade.

--- resources/views/org.blade.php
+++ resources/views/org.blade.php
@@ -21,7 +21,25 @@ include("init.inc.php"); ?>
 <hr>
 
 <div class="row justify-content-center align-items-center">
+
+    {!! Form::open(['action' => 'postsController@store', 'method' => 'POST']) !!}
+
+    <div class='form-group'>
+        {{ Form::label('title','Title') }}
+        {{ Form::text('title','',['class'=>'form-control','placeholder'=>'Title']) }}
+    </div>
+
+    <div class='form-group'>
+        {{ Form::label('body','Body') }}
+        {{ Form::textarea('body','',['id'=>'article-ckeditor','class'=>'form-control','placeholder'=>'Body Text']) }}
+    </div>
+
+    {{ Form::submit('Submit',['class'=>'btn btn-primary']) }}
+
+    {!! Form::close() !!}
+
     <form method='POST' enctype='multipart/form-data'>
+        {!! csrf_field() !!}
         Upload CSV FILE: <input type='file' name='csv_info' /> <input type='submit' name='submit' value='Upload File' />
     </form>
 <?php //include("upload.php");
